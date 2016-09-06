a1a8e499b7ade6fe41d74494a169a76343e12c8b
Alan Kleber <alankle@gmail.com>
2016-09-06 13:55:32 +0200

    ui

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -14,13 +14,11 @@
 import "phoenix_html"
 import "./stay_standalone"
 
-
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-
 
 
